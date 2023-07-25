362eb44f34e65df093639356c19577c0e5ec626d
YoannCos <yoanncos@gmail.com>
2023-07-25 15:56:29 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material';
+import Header from './components/Header';
+
+export default function MyApp() {
+  return (
+    <Container>
+      <Header
+    </Container>
+  );
+}
